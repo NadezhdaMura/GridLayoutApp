6a238cf1a6704ca9c9fad47e105339d9ae97e674
NadezhdaMura <nadezhda131313@yandex.ru>
2021-04-09 21:39:47 +0300

    dms

---
+++ app/src/main/java/com/example/gridlayoutapp/FirstFragment.kt
@@ -0,0 +1,12 @@
+package com.example.gridlayoutapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FirstFragment: Fragment(R.layout.fragment_one) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+
+}
